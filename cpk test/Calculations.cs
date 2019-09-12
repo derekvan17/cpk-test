08ebb3cdf29f2c0732e6135caedc7763ebc3e04d
eng2 <eng2@ENG2-PC>
2019-09-12 12:04:53 -0400

    Add project files.

---
+++ cpk test/Calculations.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cpk_test
+{
+    public static class Calculations
+    {
+        public static double Mean(this List<double> values)
+        {
+            return values.Count == 0 ? 0 : values.Mean(0, values.Count);
+        }
+
+        public static double Mean(this List<double> values, int start, int end)
+        {
+            double s = 0;
+
+            for (int i = start; i < end; i++)
+            {
+                s += values[i];
+            }
+
+            return s / (end - start);
+        }
+
+        public static double Variance(this List<double> values)
+        {
+            return values.Variance(values.Mean(), 0, values.Count);
+        }
+
+        public static double Variance(this List<double> values, double mean)
+        {
+            return values.Variance(mean, 0, values.Count);
+        }
+
+        public static double Variance(this List<double> values, double mean, int start, int end)
+        {
+            double variance = 0;
+
+            for (int i = start; i < end; i++)
+            {
+                variance += Math.Pow((values[i] - mean), 2);
+            }
+
+            int n = end - start;
+            //if (start > 0) n -= 1;
+
+            return variance / (n - 1);
+        }
+
+        public static double StandardDeviation(this List<double> values)
+        {
+            return values.Count == 0 ? 0 : values.StandardDeviation(0, values.Count);
+        }
+
+        public static double StandardDeviation(this List<double> values, int start, int end)
+        {
+            double mean = values.Mean(start, end);
+            double variance = values.Variance(mean, start, end);
+
+            return Math.Sqrt(variance);
+        }
+
+        public static double CpkPullTest(this List<double> values)
+        {
+            return values.Count == 0 ? 0 : values.CpkPullTest(0, values.Count);
+        }
+
+        public static double CpkPullTest(this List<double> values, int start, int end)
+        {
+            int minPullSpec = 250;
+            double mean = values.Mean(start, end);
+            double sd = values.StandardDeviation(start, end);
+
+            return (mean - minPullSpec) / (3 * sd);
+
+        }
+    }
+}
---
+++ cpk test/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace cpk_test
+{
+    public partial class Form1 : MetroForm
+    {
+        List<double> data = new List<double> {};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string[] Assign()
+        {
+            double mean = data.Mean();
+            double variance = data.Variance();
+            double sd = data.StandardDeviation();
+            double cpk = data.CpkPullTest();
+
+            string[] var = new string[5];
+            var[0] = Convert.ToString(mean);
+            var[1] = Convert.ToString(variance);
+            var[2] = Convert.ToString(sd);
+            var[3] = Convert.ToString(cpk);
+
+            return var;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            data.Add(Convert.ToDouble(textBox1.Text));
+
+            string[] var = Assign();
+            label1.Text = var[0];
+            label2.Text = var[1];
+            label3.Text = var[2];
+            label8.Text = var[3];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
